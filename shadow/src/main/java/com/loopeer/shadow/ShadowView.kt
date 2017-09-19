0e78f674df16daa083a0174a8f5d765d1fd7fe7d
ToDou <yytodou123@gmail.com>
2017-09-19 18:44:20 +0800

    apply the foreground to shadowview

--- shadow/src/main/java/com/loopeer/shadow/ShadowView.kt
+++ shadow/src/main/java/com/loopeer/shadow/ShadowView.kt
@@ -1,24 +1,22 @@
 package com.loopeer.shadow
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.widget.FrameLayout
-import android.graphics.RectF
+import android.os.Build
 
 class ShadowView @JvmOverloads constructor(context: Context?, attributeSet: AttributeSet? = null, defStyleInt: Int = 0)
     : FrameLayout(context, attributeSet, defStyleInt) {
 
     private val bgPaint = Paint()
-    var shadowColor : Int = 0
+    var shadowColor: Int = 0
     var offset = 10
-    set(value) {
-        field = value
-        invalidate()
-    }
+        set(value) {
+            field = value
+            invalidate()
+        }
+
     var shadowRadius = 7f
         set(value) {
             field = value
@@ -26,15 +24,16 @@ class ShadowView @JvmOverloads constructor(context: Context?, attributeSet: Attr
                     shadowColor)
             invalidate()
         }
+    var radius = 0f
 
     init {
-        shadowColor = Color.parseColor("#E3E5E7")
+        shadowColor = Color.parseColor("#FAC4BA")//FAC4BA E3E5E7
         bgPaint.color = Color.WHITE
         bgPaint.isAntiAlias = true
         bgPaint.style = Paint.Style.FILL
         bgPaint.setShadowLayer(shadowRadius, 0f, 1f,
                 shadowColor)
-        setLayerType(LAYER_TYPE_SOFTWARE, bgPaint)
+        setLayerType(LAYER_TYPE_SOFTWARE, null)
         setWillNotDraw(false)
     }
 
@@ -42,14 +41,39 @@ class ShadowView @JvmOverloads constructor(context: Context?, attributeSet: Attr
         super.onDraw(canvas)
 
         canvas?.let {
-            val r = 4f
+            val r = 20f
             val w = measuredWidth
             val h = measuredHeight
             val rect = Rect(offset, offset, w - offset, h - offset)
             val rectF = RectF(rect)
             it.drawRoundRect(rectF, r, r, bgPaint)
         }
-        super.onDraw(canvas)
     }
+
+    override fun onDrawForeground(canvas: Canvas?) {
+        canvas?.let {
+            canvas.save()
+            val path = Path()
+            val w = measuredWidth
+            val h = measuredHeight
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                path.addRoundRect(offset.toFloat(), offset.toFloat(), (w - offset).toFloat(), (h - offset).toFloat(), 20f, 20f, Path.Direction.CW)
+                canvas.clipPath(path)
+            }
+            super.onDrawForeground(canvas)
+            canvas.restore()
+        }
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+    }
+
 
 }
